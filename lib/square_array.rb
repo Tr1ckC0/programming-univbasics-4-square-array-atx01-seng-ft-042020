3c4ef474ea46c26d127c97778f80cd23a9a3c331
Robert Collins <rpc219@gmail.com>
2020-01-14 21:14:47 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,11 @@
 def square_array(array)
+  squared_array = []
   counter = 0
   while array[counter] do
     array[counter] = array[counter] ** 2
     counter += 1
   end
-  array
+  squared_array
 end
 
 
